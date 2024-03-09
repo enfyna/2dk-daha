11be06de197653527e865fd9acb2e1298f9bba9d
enfyna <legendraslan@gmail.com>
2024-03-09 12:48:09 +0300

    Oyun Yöneticisi ekle

---
+++ Yonetici/OyunYT.gd
@@ -0,0 +1,38 @@
+extends Node
+
+var ogrenci : Ogrenci
+var toplam_puan : int
+var YanOyunOynaniyorMu : bool = false
+
+var ToplamPuan : int = 0
+var _YanOyunPuan : float = 0.0
+
+func oyunu_baslat():
+    ogrenci = Ogrenci.new()
+    ogrenci.ayarla()
+    Saat.connect("iki_dk_gecti", iki_dk_gecti)
+    Saat.saati_baslat()
+    
+func yan_oyun_oynanmaya_basladi():
+    YanOyunOynaniyorMu = true
+    ogrenci.DersCalismaMotivasyonu = 1.0
+
+func yan_oyun_bitirildi():
+    YanOyunOynaniyorMu = false
+    var sure : float = 0.0
+    var verimlilik : float = 0.0
+    for aks : Aksiyon in ogrenci.Aksiyonlar:
+        print(aks.Ad, aks.GecirilenSure)
+        sure += aks.GecirilenSure
+        verimlilik += aks.Puan * aks.GecirilenSure
+    print("sure", sure)
+    print("verim", verimlilik)
+    var ogrenci_katsayi = verimlilik / sure
+    print("ok", ogrenci_katsayi)
+    ToplamPuan += ceili(_YanOyunPuan * ogrenci_katsayi)
+    return [sure, ogrenci_katsayi, _YanOyunPuan, ceili(_YanOyunPuan * ogrenci_katsayi)] 
+
+func iki_dk_gecti():
+    if YanOyunOynaniyorMu:
+        ogrenci.aksiyon_bitir()
+        ogrenci.aksiyon_basla()
