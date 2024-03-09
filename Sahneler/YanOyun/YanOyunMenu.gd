cbb33e6477d7038017a2d54e7120e5d8d06882f8
enfyna <legendraslan@gmail.com>
2024-03-09 16:29:31 +0300

    Akış göstergesine geçiş efekti ekle

--- Sahneler/YanOyun/YanOyunMenu.gd
+++ Sahneler/YanOyun/YanOyunMenu.gd
@@ -21,6 +21,7 @@ func _ready():
         button.connect("pressed", yan_oyun_oyna.bind(oyun))
         ButonContainer.add_child(button)
 
+    AkisOrani = AkisSayisi.get_node("AkisOrani")
     ButonContainer.remove_child(temp_button)
     ButonContainer.show()
 
@@ -29,6 +30,7 @@ func yan_oyun_oyna(oyun : Oyun):
     YanOyun = oyun
     #oyun.connect("basarili")
     #oyun.connect("hatali")
+    YanOyun.connect("akis_orani", akis_orani_guncelle)
     YanOyun.sec(
         YanOyunContainer.get_node("Soru"),
         YanOyunContainer.get_node("Cevap"),
@@ -37,9 +39,15 @@ func yan_oyun_oyna(oyun : Oyun):
     YanOyunContainer.show()
     YanOyunOynaButonu.show()
 
+func akis_orani_guncelle(zorluk : int, oran : float, renk : Color):
+    var tween = get_tree().create_tween()
+    tween.tween_property(AkisSayisi, "modulate", renk, 0.2)
+    tween.tween_property(AkisOrani, "modulate", renk, 0.2)
+    tween.tween_property(AkisOrani, "value", oran, 0.2)
+
+    AkisSayisi.text = "%s" % [zorluk]
+
 func _on_ogrenci_kontrol_et_pressed():
     OyunYT._YanOyunPuan = YanOyun.KazanilanPuan
-    print(OyunYT._YanOyunPuan)
-    print(YanOyun.KazanilanPuan)
     get_tree().change_scene_to_file("res://Sahneler/YanOyunSonuc/YanOyunSonuc.tscn")
 
