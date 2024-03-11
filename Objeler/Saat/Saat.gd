ecd487e267a38ec276cad4733151ede700add547
enfyna <legendraslan@gmail.com>
2024-03-11 17:14:33 +0300

    Saat sınıfını güncelle.

--- Objeler/Saat/Saat.gd
+++ Objeler/Saat/Saat.gd
@@ -1,10 +1,10 @@
 extends Node
 
 enum Hiz {
-    # gercek hayatta 1 saniye ...
-    Yavas = 30, # ... oyunda 30 saniye
-    Orta = 60,  # ... oyunda 60 saniye
-    Hizli = 120, # ... oyunda 120 saniye
+	# gercek hayatta 1 saniye ...
+	Yavas = 30, # ... oyunda 30 saniye
+	Orta = 60,  # ... oyunda 60 saniye
+	Hizli = 120, # ... oyunda 120 saniye
 }
 
 signal iki_dk_gecti
@@ -23,52 +23,57 @@ var OyunBasladiMi : bool = false
 var SaatStr : String = "06:00"
 
 func saati_baslat():
-    SaatNode = load("res://Objeler/Saat/Saat.tscn").instantiate()
-    SaatText = SaatNode.get_node("Text")
-    SaatText.add_theme_font_size_override("font_size", 50)
-    MotivasyonGosterge = SaatNode.get_node("Motivasyon")
-    MotivasyonGosterge.value = 0.0
-    OyunYT.ogrenci.connect("ders_motivasyonu_degisti", ogrenci_motivasyon_guncelle)
-    add_child(SaatNode)
-    OyunBasladiMi = true
+	SaatNode = load("res://Objeler/Saat/Saat.tscn").instantiate()
+	SaatText = SaatNode.get_node("Text")
+	SaatText.add_theme_font_size_override("font_size", 50)
+	MotivasyonGosterge = SaatNode.get_node("Control/Motivasyon")
+	MotivasyonGosterge.value = 0.0
+	OyunYT.ogrenci.connect("ders_motivasyonu_degisti", ogrenci_motivasyon_guncelle)
+	add_child(SaatNode)
+	OyunBasladiMi = true
 
 func _process(delta):
-    if not OyunBasladiMi:
-        return
+	if not OyunBasladiMi:
+		return
 
-    KalanZaman -= delta * OyunHizi
-    IkiDkGectiMi += delta * OyunHizi
+	KalanZaman -= delta * OyunHizi
+	if KalanZaman <= 0:
+		OyunYT.sahne_degistir("res://Sahneler/OyunSonu/OyunSonu.tscn", 1)
+		set_process(false)
+		queue_free()
 
-    if floorf(IkiDkGectiMi) > 120:
-        IkiDkGectiMi = 0.0
-        emit_signal("iki_dk_gecti")
+	IkiDkGectiMi += delta * OyunHizi
 
-    SaatStr = SaatFormat % [
-        floor(KalanZaman / 3600),    
-        floor(fmod(KalanZaman, 3600) / 60),
-    ]
+	if floorf(IkiDkGectiMi) > 120:
+		IkiDkGectiMi = 0.0
+		emit_signal("iki_dk_gecti")
 
-    SaatText.text = SaatStr 
+	SaatStr = SaatFormat % [
+		floor(KalanZaman / 3600),    
+		floor(fmod(KalanZaman, 3600) / 60),
+	]
 
-    return
+	SaatText.text = SaatStr 
+
+	return
 
 func ogrenci_motivasyon_guncelle():
-    MotivasyonTween = get_tree().create_tween()
-    MotivasyonTween.tween_property(
-        MotivasyonGosterge, "value", OyunYT.ogrenci.DersCalismaMotivasyonu, 0.1
-    )
-    MotivasyonTween.tween_property(
-        MotivasyonGosterge, "modulate", Color(
-                (1.0 - OyunYT.ogrenci.DersCalismaMotivasyonu) * 2.0,
-                OyunYT.ogrenci.DersCalismaMotivasyonu,
-                0.0,
-                1.0
-            ),
-            0.1
-    )
+	MotivasyonTween = get_tree().create_tween()
+	MotivasyonTween.tween_property(
+		MotivasyonGosterge, "value", OyunYT.ogrenci.DersCalismaMotivasyonu, 0.1
+	)
+	MotivasyonTween.tween_property(
+		MotivasyonGosterge, "modulate", Color(
+				(1.0 - OyunYT.ogrenci.DersCalismaMotivasyonu) * 2.0,
+				OyunYT.ogrenci.DersCalismaMotivasyonu,
+				0.0,
+				1.0
+			),
+			0.1
+	)
 
 func sure_azalt(saniye : int):
-    KalanZaman -= saniye
+	KalanZaman -= saniye
 
 func al():
-    return SaatStr
+	return SaatStr
